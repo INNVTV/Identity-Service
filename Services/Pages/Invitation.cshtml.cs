a38f209dff945bba0e0b0b94036f818b41c1257d
Kaz <kaz@innvtv.com>
2019-02-03 11:39:56 -0800

    Started UI for invitation acceptence

---
+++ Services/Pages/Invitation.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages
+{
+    public class InvitationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- Services/Startup.cs
+++ Services/Startup.cs
@@ -239,6 +239,7 @@ namespace IdentityService
             #endregion
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
