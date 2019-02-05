a41c52dc44552bd7c20dad128d023f1d438d5b42
Kaz <kaz@innvtv.com>
2019-02-04 21:38:11 -0800

    Added login UI

---
+++ Services/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Application.Authentication.Commands.AuthenticateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityService.Pages.Login
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public AuthenticateUserCommand AuthenticateUser { get; set; }
+
+        readonly IMediator _mediator;
+
+        public IndexModel(IServiceProvider serviceProvider)
+        {
+            _mediator = serviceProvider.GetService<IMediator>();
+        }
+
+        public void OnGet()
+        {
+            ViewData["Message"] = String.Empty;
+            AuthenticateUser = new AuthenticateUserCommand();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await _mediator.Send(AuthenticateUser);
+
+            if(!result.isSuccess)
+            {
+                ViewData["Message"] = "Incorrect credentials";
+                return Page();
+            }
+
+            return Redirect(Request.Query["url"]);
+
+        }
+    }
+}
