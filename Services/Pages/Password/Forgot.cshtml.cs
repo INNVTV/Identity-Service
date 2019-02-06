1e68d11738cd9c413de492ea1b249cdd19e0f565
Kaz <kaz@innvtv.com>
2019-02-05 18:32:10 -0800

    Stubbed out reset password views

---
+++ Services/Pages/Password/Forgot.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Password
+{
+    public class ForgotModel : PageModel
+    {
+        [BindProperty]
+        public string UserNameOrEmail { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            return Page();
+        }
+    }
+}
---
+++ Services/Pages/Password/Reset.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Password
+{
+    public class ResetModel : PageModel
+    {
+        public void OnGet(string id)
+        {
+            var _id = id;
+        }
+    }
+}
