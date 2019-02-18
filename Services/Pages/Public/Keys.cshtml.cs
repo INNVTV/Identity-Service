4fe8bb33d5cf2f302c5b2a0ec2f56e9266c65f23
Kaz <kaz@innvtv.com>
2019-02-17 18:36:53 -0800

    Added modulus/exponent to rsa public key page and api

--- Services/Pages/Public/Keys.cshtml.cs
+++ Services/Pages/Public/Keys.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using Core.Infrastructure.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -21,6 +22,19 @@ namespace IdentityService.Pages.Public
         {
             ViewData["XMLString"] = _coreConfiguration.JSONWebTokens.PublicKeyXmlString;
             ViewData["PEM"] = _coreConfiguration.JSONWebTokens.PublicKeyPEM;
+
+            // Extract Modulus and Exponent from XMLString
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(_coreConfiguration.JSONWebTokens.PublicKeyXmlString);
+
+            foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+            {
+                switch (node.Name)
+                {
+                    case "Modulus": ViewData["Modulus"] = node.InnerText; break;
+                    case "Exponent": ViewData["Exponent"] = node.InnerText; break;
+                }
+            }
         }
     }
 }
